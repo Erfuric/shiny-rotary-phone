55c0d167762875b57fe9f053497bd15708363a0a
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-05 19:25:42 +1100

    Port testing

--- Develop/server.js
+++ Develop/server.js
@@ -8,10 +8,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Turning on routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('App listening on port' + PORT));
+  app.listen(PORT, () => console.log('Now listening'));
 });
